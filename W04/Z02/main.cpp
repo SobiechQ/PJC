78e96b9f41886a8155626927f5e3d7aeaa503bd4
Mikołaj <mikolajmikolaj090302@gmail.com>
2023-03-27 20:30:01 +0200

    Grupowanie po długości tekstu

--- W04/Z02/main.cpp
+++ W04/Z02/main.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <fmt/ranges.h>
 #include <set>
+#include <map>
+
 std::vector<std::string> getNamesFrom(std::vector<std::string> input){
     std::vector<std::string> output = input;
     erase_if(output,[](std::string s)->bool{
@@ -18,6 +20,12 @@ std::vector<std::string> filtered(std::vector<std::string> input, std::set<std::
         redacted.contains(s);
     });
     return input;
+}
+std::map<int, std::vector<std::string>> group(std::vector<std::string> input){
+    std::map<int, std::vector<std::string>> output;
+    for(std::string s:input)
+        output[s.size()].push_back(s);
+    return output;
 }
 
 int main() {
@@ -34,6 +42,13 @@ int main() {
         //Filtrowanie Znaków
         std::set<string> redactedWords = {"Cpp", "Java", "Go", ".NET", "COBOL"};
         fmt::print("{}", filtered(std::vector<string>{"Tofu", "Onigiri", "COBOL", "Maki", "Java"}, redactedWords));
+    }
+*/
+/*
+    {
+//        grupowanie po długości tekstu
+        std::vector<string> message = {"Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua.", "Ut", "enim", "ad", "minim", "veniam,", "quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea", "commodo", "consequat"};
+        fmt::print("{} \n",group(message));
     }
 */
 
