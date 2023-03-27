53596eae3e9ffb7209d6de75c7800e04b396d4fe
Mikołaj <mikolajmikolaj090302@gmail.com>
2023-03-27 20:03:37 +0200

    Filtrowanie Znaków

--- W04/Z02/main.cpp
+++ W04/Z02/main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <fmt/ranges.h>
+#include <set>
 std::vector<std::string> getNamesFrom(std::vector<std::string> input){
     std::vector<std::string> output = input;
     erase_if(output,[](std::string s)->bool{
@@ -12,11 +13,30 @@ std::vector<std::string> getNamesFrom(std::vector<std::string> input){
     });
     return output;
 }
+std::vector<std::string> filtered(std::vector<std::string> input, std::set<std::string> redacted){
+    std::erase_if(input, [redacted](std::string s) -> bool {
+        redacted.contains(s);
+    });
+    return input;
+}
 
 int main() {
     using namespace std;
-    vector<string> wyrazy = {"Jan","dzban","Mann","90lan","3384","JAKTO","c++"};
-    fmt::print("{}",getNamesFrom(wyrazy));
+    /*
+    {
+        //Filtrowanie Imion
+        vector<string> wyrazy = {"Jan","dzban","Mann","90lan","3384","JAKTO","c++"};
+        fmt::print("{}",getNamesFrom(wyrazy));
+    }
+    */
+/*
+    {
+        //Filtrowanie Znaków
+        std::set<string> redactedWords = {"Cpp", "Java", "Go", ".NET", "COBOL"};
+        fmt::print("{}", filtered(std::vector<string>{"Tofu", "Onigiri", "COBOL", "Maki", "Java"}, redactedWords));
+    }
+*/
+
 
 
 
---
+++ W04/Z03/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by mikol on 27.03.2023.
+//
+#include <iostream>
+int main() {
+    using namespace std;
+    
+    return 0;
+}
