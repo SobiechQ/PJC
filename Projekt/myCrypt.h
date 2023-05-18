8562c1d601ef7d124a180c967ef6874ce16b1475
Mikołaj <mikolajmikolaj090302@gmail.com>
2023-05-18 14:56:59 +0200

    my crypt

---
+++ Projekt/myCrypt.h
@@ -0,0 +1,8 @@
+//
+// Created by mikol on 18.05.2023.
+//
+
+#ifndef PJC2_MYCRYPT_H
+#define PJC2_MYCRYPT_H
+
+#endif //PJC2_MYCRYPT_H
---
+++ Projekt/test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by mikol on 18.05.2023.
+//
+#include <iostream>
+#include <fmt/printf.h>
+#include "myCrypt.cpp"
+int main() {
+    using namespace std;
+    enc2("hello", "there");
+
+
+
+
+    return 0;
+}
